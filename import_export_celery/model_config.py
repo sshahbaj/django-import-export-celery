01b8a5f16034ba5f7b11cc8734382f575be83747
Shahbaj Sayyad <shahbajsayyad.s@gmail.com>
2020-05-20 19:26:38 +0530

    Hardcode resource import

--- import_export_celery/model_config.py
+++ import_export_celery/model_config.py
@@ -1,6 +1,7 @@
 from django.apps import apps
 from import_export.resources import modelresource_factory
 from celery.utils.log import get_task_logger
+from leads.resource import get_lead_resource
 
 log = get_task_logger(__name__)
 
@@ -9,7 +10,4 @@ class ModelConfig:
     def __init__(self, app_label=None, model_name=None, resource=None):
         self.model = apps.get_model(app_label=app_label, model_name=model_name)
         log.debug(resource)
-        if resource:
-            self.resource = resource()
-        else:
-            self.resource = modelresource_factory(self.model)
+        self.resource = get_lead_resource()
